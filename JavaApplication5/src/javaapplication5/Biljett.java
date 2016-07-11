8d0e817d06c824a84fce3713fd67c8bd48573e24
User <User@94Z7J32>
2016-07-11 13:32:47 +0200

    Hej

---
+++ JavaApplication5/src/javaapplication5/Biljett.java
@@ -0,0 +1,14 @@
+package javaapplication5;
+
+/**
+ *
+ * @author User
+ */
+public class Biljett {
+    int plats;
+    int pris;
+    boolean mat;
+    String matRatt;
+    String flight;
+    String forNamn;
+}
