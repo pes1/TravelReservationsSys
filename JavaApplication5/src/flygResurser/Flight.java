8090f02fe3b6bf707e439707a5ef9c228ec0ca94
user <cjobbcul@gmail.com>
2016-07-12 14:35:06 +0200

    Merge origin/master
    Conflicts:
    	.gitignore
    	JavaApplication5/src/flygResurser/Biljett.java
    	JavaApplication5/src/flygResurser/Flight.java

--- JavaApplication5/src/flygResurser/Flight.java
+++ JavaApplication5/src/flygResurser/Flight.java
@@ -12,13 +12,17 @@ public class Flight {
     private List<Biljett> bokadeBijetter;
     private Map<Integer, Boolean> platser;
     private int flightNumber;
+    private int prisFirst;
+    private int prisSecond;
     
-    public Flight(String destination, String avreseort, List<Biljett> bokadeBijetter, Map<Integer, Boolean> platser, int flightNumber){
+    public Flight(String destination, String avreseort, List<Biljett> bokadeBijetter, Map<Integer, Boolean> platser, int flightNumber, int prisFirst, int prisSecond){
         this.destination = destination;
         this.avresesort = avreseort;
         this.bokadeBijetter = bokadeBijetter;
         this.platser = platser;
         this.flightNumber = flightNumber;
+        this.prisFirst = prisFirst;
+        this.prisSecond = prisSecond;
     }
     
     public void fyllmap() {
@@ -28,6 +32,12 @@ public class Flight {
     }
     
     public int getPlats() {
+        for(int i = 1;i<=10;i++) {
+            if(platser.get(i) == false) {
+                platser.put(i, true);
+                return i;
+            }
+        }
         return 0;
     }
 }
