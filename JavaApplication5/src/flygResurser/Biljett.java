4c81232260a1ee39ff1c031b8d3673700c047602
User (PES) <entermosjava@gmail.com>
2016-07-12 13:39:23 +0200

    test commit

--- JavaApplication5/src/flygResurser/Biljett.java
+++ JavaApplication5/src/flygResurser/Biljett.java
@@ -1,105 +1,43 @@
-package javaapplication5;
+package flygResurser;
+
+import resurser.FlygMatMenyer;
 
 public class Biljett {
     private int plats;
     private int pris;
     private int klass;
     private boolean mat;
-    private Pair matRatt;
+    private FlygMatMenyer matRatt;
     private String flight;
     private String forNamn;
     private String efterNamn;
     //private int kundNummer;
 
-    public Biljett(int plats, int klass, boolean mat, String matRatt, String flight,
+    public Biljett(int plats, int klass, boolean mat, FlygMatMenyer matRatt, String flight,
             String forNamn, String efterNamn){
         this.plats = plats;
         this.pris = setPris(plats, mat, matRatt);
         this.klass = klass;
         this.mat = mat;
-        if (mat)this.matRatt = matRatt;
-        else this.matRatt = null;    
+//        if (mat)
+//            this.matRatt = matRatt;
+//        else this.matRatt = null;    
         this.flight = flight;
         this.forNamn = forNamn;
         this.efterNamn = efterNamn;
     }
     
-    public int setPris(int plats, boolean mat, Pair matRatt){
+    public int setPris(int plats, boolean mat, FlygMatMenyer matRatt){
         int biljettPris = 0;
         
         //1a eller 2a klass
         if (plats < 6) biljettPris += 20000;
         else biljettPris += 5000;
         
-        if(mat) biljettPris += matRatt.getPris();
+       // if(mat) biljettPris += matRatt.getFood();
         
         return biljettPris;
     }
-
-    public int getPlats() {
-        return plats;
-    }
-
-    public int getPris() {
-        return pris;
-    }
-
-    public int getKlass() {
-        return klass;
-    }
-
-    public boolean isMat() {
-        return mat;
-    }
-
-    public Pair getMatRatt() {
-        return matRatt;
-    }
-
-    public String getFlight() {
-        return flight;
-    }
-
-    public String getForNamn() {
-        return forNamn;
-    }
-
-    public String getEfterNamn() {
-        return efterNamn;
-    }
-
-    public void setPlats(int plats) {
-        this.plats = plats;
-    }
-
-    public void setPris(int pris) {
-        this.pris = pris;
-    }
-
-    public void setKlass(int klass) {
-        this.klass = klass;
-    }
-
-    public void setMat(boolean mat) {
-        this.mat = mat;
-    }
-
-    public void setMatRatt(Pair matRatt) {
-        this.matRatt = matRatt;
-    }
-
-    public void setFlight(String flight) {
-        this.flight = flight;
-    }
-
-    public void setForNamn(String forNamn) {
-        this.forNamn = forNamn;
-    }
-
-    public void setEfterNamn(String efterNamn) {
-        this.efterNamn = efterNamn;
-    }
-    
     
     
 }
--- JavaApplication5/src/flygResurser/Flight.java
+++ JavaApplication5/src/flygResurser/Flight.java
@@ -1,11 +1,10 @@
-package javaapplication5;
+package flygResurser;
 
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import javaapplication5.Biljett;
+import flygResurser.Biljett;
 
 public class Flight {
     private String destination;
@@ -14,52 +13,21 @@ public class Flight {
     private Map<Integer, Boolean> platser;
     private int flightNumber;
     
-    public Flight(){
-        bokadeBijetter = new ArrayList<Biljett>();
-        
-    }
-
-    //public getLedigaPlatser(){}
-    
-    public String getDestination() {
-        return destination;
-    }
-
-    public String getAvresesort() {
-        return avresesort;
-    }
-
-    public List<Biljett> getBokadeBijetter() {
-        return bokadeBijetter;
-    }
-
-    public Map<Integer, Boolean> getPlatser() {
-        return platser;
-    }
-
-    public int getFlightNumber() {
-        return flightNumber;
-    }
-
-    public void setDestination(String destination) {
+    public Flight(String destination, String avreseort, List<Biljett> bokadeBijetter, Map<Integer, Boolean> platser, int flightNumber){
         this.destination = destination;
-    }
-
-    public void setAvresesort(String avresesort) {
-        this.avresesort = avresesort;
-    }
-
-    public void setBokadeBijetter(List<Biljett> bokadeBijetter) {
+        this.avresesort = avreseort;
         this.bokadeBijetter = bokadeBijetter;
-    }
-
-    public void setPlatser(Map<Integer, Boolean> platser) {
         this.platser = platser;
-    }
-
-    public void setFlightNumber(int flightNumber) {
         this.flightNumber = flightNumber;
     }
     
+    public void fyllmap() {
+        for(int i=1;i<=10;i++) {
+            platser.put(i,false);
+        }
+    }
     
+    public int getPlats() {
+        return 0;
+    }
 }
